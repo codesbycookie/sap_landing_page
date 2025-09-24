c43844e009792ef640429afba6e2900bde06415e
rajkaran <rajkaran1505@gmail.com>
2025-09-24 23:58:24 +0530

    Font Added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <>
     
-    <h1>Hello</h1>
+    <h1 className='urbanist-900 text-3xl'>Hello</h1>
+    <h1 className='urbanist-700 text-3xl'>Hello</h1>
+    <h1 className='urbanist-500 text-3xl'>Hello</h1>
     </>
   )
 }
