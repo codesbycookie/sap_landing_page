790921e685ea00a961b8acd72dc46a910e9f75b9
rajkaran <rajkaran1505@gmail.com>
2025-09-27 12:14:30 +0530

    Gallery Page Done

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Gallery() {
   return (
     <div className="h-screen">
-      <div className="flex flex-col justify-center items-center my-5">
-      <p className='text-lg text-gray-400'>Belive in your Potential</p>
-      <h1 className='text-4xl text-green-500 font-semibold'>Ambience of Sampath Academy</h1>
-      <p className='text-lg text-gray-400'>Each result is more than a number — it’s a story of hard work, growth, and dedication.</p>
+      <div className="flex flex-col item-start justify-center md:items-center my-5 p-5">
+      <p className='text-md md:text-lg text-gray-400'>Belive in your Potential</p>
+      <h1 className='text-2xl md:text-4xl text-green-500 font-semibold'>Ambience of Sampath Academy</h1>
+      <p className='text-md md:text-lg text-gray-400'>Each result is more than a number — it’s a story of hard work, growth, and dedication.</p>
       </div>
       <div className="flex h-full w-full items-center justify-center">
         <div className="grid h-full w-full gap-4 p-2 grid-cols-4 grid-rows-7 rounded-lg shadow-md">
